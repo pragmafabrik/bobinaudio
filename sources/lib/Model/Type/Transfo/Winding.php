a1a7366eb4a8bdd42198ae8b98bc9ce06836889e
Grégoire HUBERT <hubert.greg@gmail.com>
2014-06-11 08:38:08 +0200

    Impedance in windings

--- sources/lib/Model/Type/Transfo/Winding.php
+++ sources/lib/Model/Type/Transfo/Winding.php
@@ -8,10 +8,18 @@ class Winding extends Composite
 {
     public $current;
     public $voltage;
+    public $impedance;
 
     public function __toString()
     {
-        return sprintf("%02.1fV / %02.3fA", $this->voltage / 10, $this->current / 1000);
+        $current = ($this->current >= 1000) ?  sprintf("%02.1fA", $this->current / 1000) : sprintf("%dmA", $this->current);
+        $voltage = ($this->voltage % 10 == 0) ? sprintf("%dV", $this->voltage / 10) : sprintf("%02.1fV", $this->voltage / 10);
+        if (!is_null($this->impedance))
+        {
+            $impedance = ($this->impedance >= 10) ? sprintf("%dΩ", $this->impedance / 10) : sprintf("%02.1fΩ", $this->impedance / 10);
+        }
+
+        return sprintf("%s / %s%s", $voltage, $current, isset($impedance) ? sprintf(" / %s", $impedance) : '');
     }
 }
 
